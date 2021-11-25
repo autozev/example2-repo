343e6ac1d77e0512addef6a8e890f33dbd14a384
autozev <autozev@yandex.ru>
2021-11-25 17:07:52 +0300

    2nd commit

---
+++ app/src/main/java/study/eduard/my2ndapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package study.eduard.my2ndapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ;ouhoihlkjh
+        ;jn;nlknnnl
+        kklljh;ookj;lkn;o;jjjjjh
+
+    }
+}
